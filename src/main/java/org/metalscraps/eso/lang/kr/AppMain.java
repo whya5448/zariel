df1f7f8659c2cf8234d82f6613a2acb2322b8177
harion01 <harion01@gmail.com>
2018-02-10 18:42:54 +0900

    csv txt로 만드는기능 추가

--- src/main/java/org/metalscraps/eso/lang/kr/AppMain.java
+++ src/main/java/org/metalscraps/eso/lang/kr/AppMain.java
@@ -35,7 +35,7 @@ public class AppMain {
 		System.out.println("11. TTC");
 		System.out.println("12. Destinations");
 		System.out.println("100. PO -> 구글 번역 (beta)");
-		System.out.println("200. PO -> 번역가버전 생성 (파일명 앞에 추가)");
+		System.out.println("200. csv -> text");
 
 	}
 
@@ -87,6 +87,7 @@ public class AppMain {
 				case 11: new TamrielTradeCentre(appWorkConfig).start();
 				case 12: new Destinations(appWorkConfig).start();
 				case 100: lm.translateGoogle(); break;
+                case 200: lm.CsVToText(); break;
 
 			}
 		}
