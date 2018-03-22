8e4b3429ff82f00db668a41fc1a4f152836fe4b7
Chris Lee <jjlee@bistel.com>
2018-03-22 18:58:26 +0900

    USEP 웹크롤링 기능 추가 - skill page (todo : 파싱)

---
+++ src/main/java/org/metalscraps/eso/lang/kr/Utils/CategoryGenerator.java
@@ -0,0 +1,17 @@
+package org.metalscraps.eso.lang.kr.Utils;
+
+import org.apache.http.client.ClientProtocolException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.metalscraps.eso.lang.kr.bean.WebData;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CategoryGenerator {
+
+}
+
+
