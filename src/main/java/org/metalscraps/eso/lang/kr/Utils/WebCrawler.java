8b636eaaaded58769a4529ca512b67aabd36a5b1
harion01 <harion01@gmail.com>
2018-03-27 22:48:23 +0900

    Web SkillParser - 챔피언 포인트, 스킬트리 파싱 추가

--- src/main/java/org/metalscraps/eso/lang/kr/Utils/WebCrawler.java
+++ src/main/java/org/metalscraps/eso/lang/kr/Utils/WebCrawler.java
@@ -17,110 +17,151 @@ import org.metalscraps.eso.lang.kr.bean.WebData;
 //get html table data from url.
 public class WebCrawler {
 
-    public boolean GetUESPSkillTree(WebData outputData){
+    public boolean GetUSEPWebPage(WebData PageData, String PageName, int PageLoop) throws IOException {
+        String url;
+        url = "https://esoitem.uesp.net/viewlog.php?record=" + PageName;
+        System.out.println(url);
+        Document HTMLdoc = Jsoup.connect(url).get();
+        Element table = HTMLdoc.select("table").get(0);
+        PageData.addWebTable(table);
+
+
+        for (int pageCount = 1; pageCount < PageLoop; pageCount++) {
+            url = "https://esoitem.uesp.net/viewlog.php?start=" + pageCount * 300 + "&record=" + PageName;
+            System.out.println(url);
+            HTMLdoc = Jsoup.connect(url).get();
+            table = HTMLdoc.select("table").get(0);
+            PageData.addWebTable(table);
+        }
+        if(PageData.getWebTables() == null){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+
+    public boolean GetUESPSkillTree(ArrayList<CategoryCSV> SkillCSV){
         boolean ret = false;
         try {
-            Document HTMLdoc = Jsoup.connect("https://esoitem.uesp.net/viewlog.php?record=skillTree").get();
-            // https://esoitem.uesp.net/viewlog.php?start=300&record=skillTree
-            Element table = HTMLdoc.select("table").get(0);
-            outputData.addWebTable(table);
-
-            String url;
-            for(int pageCount = 1; pageCount < 0 ; pageCount++) {
-                url = "https://esoitem.uesp.net/viewlog.php?start="+pageCount*300+"&record=skillTree";
-                System.out.println(url);
-                HTMLdoc = Jsoup.connect(url).get();
-                // https://esoitem.uesp.net/viewlog.php?start=300&record=skillTree
-                table = HTMLdoc.select("table").get(0);
-                outputData.addWebTable(table);
+            WebData USEPSkillData = new WebData();
+            ret = GetUSEPWebPage(USEPSkillData, "skillTree", 9);
+            if(ret) {
+                ret = ParseUSEPSkillTable(USEPSkillData, SkillCSV);
             }
-            ret = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
 
+    private boolean ParseUSEPSkillTable(WebData USEPWebData, ArrayList<CategoryCSV> skillCSV) {
+        Set<String> SkillCategory = new HashSet<>();
+        CategoryCSV CCSV = null;
 
-
-            Set<String> SkillCategory = new HashSet<String>();
-            CategoryCSV CCSV = null;
-            ArrayList<CategoryCSV> SkillCSV = new ArrayList<CategoryCSV>();
-            boolean sr = false;
-            for(Element oneTable : outputData.getWebTables()) {
-
-                Elements skills = oneTable.select("tr");
-                for(Element skill : skills){
-
-                    System.out.println("--------------------------------------");
-                    //System.out.println("one row" + skill.html());
-                    Elements cols = skill.select("td");
-                    if(cols.size() > 0) {
-                        System.out.println("skill id :" + cols.get(2).text() + " category :" + cols.get(4).text() + " skill name :" + cols.get(6).text() );
-                        sr = SkillCategory.add(cols.get(4).text());
-                        if(sr){
-                            if(CCSV!=null) {
-                                System.out.println("prev Set inserted : "+CCSV.getCategory());
-                                SkillCSV.add(CCSV);
-                            }
-                            CCSV = new CategoryCSV();
-                            CCSV.setCategory(cols.get(4).text());
-                        }
-                        //skill name poindex
-                        CCSV.addPoIndex("198758357-0-"+cols.get(2).text());
-                        //skill desc poindex
-                        CCSV.addPoIndex("132143172-0-"+cols.get(2).text());
+        boolean sr;
+        String Category;
+        for(Element oneTable : USEPWebData.getWebTables()) {
+            Elements skills = oneTable.select("tr");
+            for(Element skill : skills){
+                //System.out.println("--------------------------------------");
+                //System.out.println("one row" + skill.html());
+                Elements cols = skill.select("td");
+                if(cols.size() > 0) {
+                    //System.out.println("skill id :" + cols.get(2).text() + " category :" + cols.get(4).text() + " skill name :" + cols.get(6).text() );
+                    Category = cols.get(4).text();
+                    if(Category.length() >0) {
+                        sr = SkillCategory.add(Category);
+                    }else{
+                        continue;
                     }
+                    if(sr){
+                        if(CCSV!=null) {
+                            //System.out.println("prev Set inserted : "+CCSV.getCategory());
+                            skillCSV.add(CCSV);
+                        }
+                        CCSV = new CategoryCSV();
+                        CCSV.setCategory(cols.get(4).text());
+                    }
+                    //skill name poindex
+                    CCSV.addPoIndex("198758357-0-"+cols.get(2).text());
+                    //skill desc poindex
+                    CCSV.addPoIndex("132143172-0-"+cols.get(2).text());
                 }
             }
+        }
+        skillCSV.add(CCSV);
 
+        if(skillCSV.size() > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean GetUESPChampionSkill(ArrayList<CategoryCSV> SkillCSV){
+        boolean ret = false;
+        try {
+            WebData USEPSkillData = new WebData();
+            ret = GetUSEPWebPage(USEPSkillData, "cpSkills", 0);
+            if(ret) {
+                ret = ParseUSEPChampionSkillTable(USEPSkillData, SkillCSV);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    private boolean ParseUSEPChampionSkillTable(WebData USEPWebData, ArrayList<CategoryCSV> skillCSV) {
+        CategoryCSV CCSV = null;
+        CCSV = new CategoryCSV();
+        CCSV.setCategory("Champion::Champion Point");
+        for(Element oneTable : USEPWebData.getWebTables()) {
+            Elements skills = oneTable.select("tr");
+            for(Element skill : skills){
+                Elements cols = skill.select("td");
+                if(cols.size() > 0) {
+                    //skill name poindex
+                    CCSV.addPoIndex("198758357-0-"+cols.get(2).text());
+                    //skill desc poindex
+                    CCSV.addPoIndex("132143172-0-"+cols.get(2).text());
+                }
+            }
+        }
+        skillCSV.add(CCSV);
+
+        if(skillCSV.size() > 0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        boolean skillret;
+        ArrayList<CategoryCSV> SkillCSV = new ArrayList<>();
+
+        WebCrawler wc = new WebCrawler();
+        skillret = wc.GetUESPChampionSkill(SkillCSV);
+        skillret = wc.GetUESPSkillTree(SkillCSV);
+
+        if(skillret){
             System.out.println("SkillCSV Size : "+SkillCSV.size());
-
             for(CategoryCSV oneCSV : SkillCSV){
                 System.out.println("=========================================");
                 System.out.println("Category : "+oneCSV.getCategory());
                 for(String index: oneCSV.getPoIndexList() ){
-                    System.out.println("Index : "+ index);
+                    System.out.println(index);
                 }
-                System.out.println("=========================================");
-            }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("size of WebTable:"+outputData.getWebTables().size());
-        }
-
-        return ret;
-    }
-
-    public static void main(String[] args) throws ClientProtocolException, IOException {
-
-        WebCrawler wc = new WebCrawler();
-
-        // 1. 가져오기전 시간 찍기
-        System.out.println(" Start Date : " + getCurrentData());
-        WebData data = new WebData();
-        wc.GetUESPSkillTree(data);
-        /*
-        Document doc2 = Jsoup.connect("https://esoitem.uesp.net/viewlog.php?sort=skillTypeName&sortorder=d&record=skillTree").get();
-
-        https://esoitem.uesp.net/viewlog.php?start=300&sort=skillTypeName&sortorder=d&record=skillTree
-        //System.out.println(doc2.html());
-        System.out.println("======================================");
-        Element table = doc2.select("table").get(0);
-        System.out.println(table.html());
-        System.out.println("======================================");
-
-        Elements skills = table.select("tr");
-
-        for(Element skill : skills){
-            System.out.println("--------------------------------------");
-            //System.out.println("one row" + skill.html());
-            Elements cols = skill.select("td");
-            if(cols.size() > 0) {
-                System.out.println("skill id :" + cols.get(2).text() + " category :" + cols.get(4).text() + " skill name :" + cols.get(6).text() );
             }
         }
-        */
 
-        System.out.println(" End Date : " + getCurrentData());
+
+
+
 
     }
 
