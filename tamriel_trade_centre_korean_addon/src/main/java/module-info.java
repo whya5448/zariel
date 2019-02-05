13b0d3d301241fb61a1d9af3672829ec8ce168d2
whya5448 <whya5448@gmail.com>
2019-02-05 19:29:06 +0900

    TTC 모듈 추가(미작동)

---
+++ tamriel_trade_centre_korean_addon/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 안병길 on 2019-02-03.
+ * Whya5448@gmail.com
+ */
+module org.metalscraps.eso.lang.tamriel.trade.centre.korean.addon {
+	requires org.metalscraps.eso.lang.library;
+	requires kotlin.stdlib;
+}
