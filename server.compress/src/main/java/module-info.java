56df315c2bbb2fa0ea0398b7c2b544ff3bfe6d7b
whya5448 <whya5448@gmail.com>
2019-04-20 17:25:37 +0900

    merged

--- server.compress/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-open module org.metalscraps.eso.lang.server.compress {
-    requires org.metalscraps.eso.lang.library;
-    requires slf4j.api;
-    requires kotlin.stdlib;
-    requires spring.boot.autoconfigure;
-    requires spring.boot;
-    requires spring.beans;
-    requires spring.context;
-}
