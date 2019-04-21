689d41eed57f9df0970711f26b2611c1718bd7bd
작은게이 <whya5448@gmail.com>
2019-04-22 00:02:31 +0900

    디렉토리 명 fix

--- Dockerfiles/run.cmd
+++ Dockerfiles/run.cmd
@@ -1,2 +1,2 @@
 mkdir temp
-docker run --rm -ti -v C:\Users\whya5\IdeaProjects\zariel:/root/zariel -v %cd%\temp:/root/eso_server whya5448/dc-eso-kr
+docker run --rm -ti -v C:\Users\whya5\IdeaProjects\zariel:/root/zariel -v %cd%\dc-eso-kr:/root/eso_server whya5448/dc-eso-kr
