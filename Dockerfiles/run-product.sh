2756f84707b8a7ec3b61ffc569901a84659ea22d
작은게이 <whya5448@gmail.com>
2019-04-22 19:36:30 +0900

    id_rsa 부정식

--- Dockerfiles/run-product.sh
+++ Dockerfiles/run-product.sh
@@ -2,7 +2,7 @@
 nm=eso-product
 mkdir dc-eso-kr/Addons/Destinations/ -p
 
-if [[ -f "id_rsa" ]] ; then echo "need id_rsa" ; exit 1 ; fi
+if [[ ! -f "id_rsa" ]] ; then echo "need id_rsa" ; exit 1 ; fi
 docker create -m 600m --memory-swap=1G --name $nm --rm -ti -v $PWD/dc-eso-kr:/root/eso_server whya5448/dc-eso-kr:product
 docker cp id_rsa ${nm}:/root/.ssh/
 docker start -ai ${nm}
