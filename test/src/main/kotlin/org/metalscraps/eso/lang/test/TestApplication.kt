f56f072dcde3fbb15e7be675e42464fe9333e904
작은게이 <whya5448@gmail.com>
2019-04-29 02:11:50 +0900

    테스트코드 정리해둠

--- test/src/main/kotlin/org/metalscraps/eso/lang/test/TestApplication.kt
+++ test/src/main/kotlin/org/metalscraps/eso/lang/test/TestApplication.kt
@@ -3,28 +3,12 @@ package org.metalscraps.eso.lang.test
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.nio.file.FileSystem
-import java.nio.file.FileSystems
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.spi.FileSystemProvider
 
 @SpringBootApplication
 class TestApplication : CommandLineRunner {
 
 
     override fun run(vararg args: String?) {
-        val loader = javaClass.classLoader
-        println("1 "+ loader.getResource("7zCon.sfx")?.path)
-        println("3 "+ loader.getResource("./7zCon.sfx")?.path)
-        val x = FileSystems.getDefault().provider()
-        println(x)
-        println(x.scheme)
-        println(x.javaClass)
-        println(FileSystemProvider.installedProviders())
-        println(System.getProperty("os.name"))
-        println(System.getProperty("os.arch"))
-        println(System.getProperty("os.version"))
 
     }
 
