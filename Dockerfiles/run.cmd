f5ca025fecd294291a6cd4996a5abbb016bdd4d2
whya5448 <whya5448@gmail.com>
2019-05-09 23:48:09 +0900

    개발환경은 지속적으로 사용

--- Dockerfiles/run.cmd
+++ Dockerfiles/run.cmd
@@ -1,2 +1,3 @@
 mkdir dc-eso-kr
-docker run --rm -ti -v C:\Users\whya5\IdeaProjects\zariel:/root/zariel -v %cd%\dc-eso-kr:/root/eso_server whya5448/dc-eso-kr
+docker run --name dc-eso-kr-dev -ti -v C:\Users\whya5\IdeaProjects\zariel:/root/zariel -v %cd%\dc-eso-kr:/root/eso_server whya5448/dc-eso-kr
+docker start --name dc-eso-kr-dev -ti -v C:\Users\whya5\IdeaProjects\zariel:/root/zariel -v %cd%\dc-eso-kr:/root/eso_server whya5448/dc-eso-kr
