dc7a6dfb3657716aac3f8b85e08848f7939f5083
whya5448 <whya5448@gmail.com>
2019-05-15 03:23:39 +0900

    로그명 변경

--- test/src/main/java/module-info.java
+++ test/src/main/java/module-info.java
@@ -2,4 +2,5 @@ open module org.metalscraps.eso.lang.test {
     requires spring.boot.autoconfigure;
     requires spring.boot;
     requires kotlin.stdlib;
+	requires org.metalscraps.eso.lang.library;
 }
