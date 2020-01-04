5b4516a947c540ede432a1dbf011d895edd4f05f
작은게이 <whya5448@gmail.com>
2020-01-05 01:24:36 +0900

    nginx 미사용

--- Dockerfiles/run-nginx.cmd
+++
@@ -1,1 +0,0 @@
-docker run --rm --name nginx -p 80:80 -v %cd%/nginx/nginx.conf:/etc/nginx/nginx.conf:ro -v %cd%/dc-eso-kr:/usr/share/nginx/html:ro -d nginx:mainline
--- Dockerfiles/run-nginx.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm --name nginx -p 80:80 -v nginx.conf:/etc/nginx/nginx.conf:ro -v $PWD/dc-eso-kr:/usr/share/nginx/html:ro -d nginx:mainline
