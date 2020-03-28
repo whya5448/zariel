c7537ddc3c7bb5394dd678b5faff7ab3bc2b1d20
작은게이 <whya5448@gmail.com>
2020-03-29 02:58:17 +0900

    미사용 모듈

--- tool/src/main/java/org/metalscraps/eso/lang/tool/vo/Category.kt
+++
@@ -1,9 +0,0 @@
-package org.metalscraps.eso.lang.tool.vo
-
-class Category(val resourceId:Int, val resourceName:String, var category:String = resourceName, val volume:Int =0) {
-    override fun toString(): String {
-        return "Category(resourceName='$resourceName', category='$category')"
-    }
-}
-
-
